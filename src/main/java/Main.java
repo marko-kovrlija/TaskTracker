f1353098efad6b25f49b1f8020e41da26feb7bd3
Marko Kovrlija <kovrlija.marko.it@gmail.com>
2026-08-03 02:34:56 +0200

    New panels

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,13 +1,28 @@
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
 public class Main {
     public static void main(String[] args) {
 
+        SwingUtilities.invokeLater(()->{
+            JFrame mainFrame = new JFrame("Moj prvi swing frame!");
+            mainFrame.setSize(500, 500);
+            mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
 
+            JPanel centerPanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
+
+            JButton sacuvajBtn = new JButton("Sačuvaj");
+            JButton otkaziBtn = new JButton("Otkaži");
+            centerPanel.add(sacuvajBtn);
+            centerPanel.add(otkaziBtn);
+
+            mainFrame.setLayout(new BorderLayout(5, 5));
+
+            mainFrame.add(centerPanel, BorderLayout.CENTER);
+
+            mainFrame.setVisible(true);
+        });
 
     }
 }
